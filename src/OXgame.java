24fd6c75d2ac657b1f4be353a6aeed9287da4779
kkhgm2 <pma225jkoi7265@yahoo.co.jp>
2019-09-01 16:46:08 +0900

    update if文、チェック内容変更

--- src/OXgame.java
+++ src/OXgame.java
@@ -11,21 +11,13 @@ import java.util.Scanner;
 
 public class OXgame {
 	public static void  main(String[] args) {
+//		OX 枠の確保！
 		String[][] field = new String [3][3];
 		for(int y = 0; y < 3; y++) {
 			for(int x = 0; x < 3; x++) {
 				field[y][x] = " ";
 			}
 		}
-//		field[0][0] = " ";
-//		field[0][1] = " ";
-//		field[0][2] = " ";
-//		field[1][0] = " ";
-//		field[1][1] = " ";
-//		field[1][2] = " ";
-//		field[2][0] = " ";
-//		field[2][1] = " ";
-//		field[2][2] = " ";
 
 		System.out.println("あなたからスタートです。　０〜２までの数字を入力してください");
 
@@ -83,8 +75,7 @@ public class OXgame {
 
 	public static String judge(String[][] field, String obj) {
 		String status;
-		if((field[0][0] == obj && field[0][1] == obj && field[0][2] == obj) ||(field[1][0] == obj && field[1][1] == obj && field[1][2] == obj)||(field[2][0] == obj && field[2][1] == obj && field[2][2] == obj) ||
-			(field[0][0] == obj && field[1][0] == obj && field[2][0] == obj) || (field[0][1] == obj && field[1][1] == obj && field[2][1] == obj) || (field[0][2] == obj && field[1][2] == obj && field[2][2] == obj) ||
+		if( horaizontalCheck(field, obj) || varticalCheck(field, obj) ||
 			(field[0][0] == obj && field[1][1] == obj && field[2][2] == obj) || (field[0][2] == obj && field[1][1] == obj && field[2][0] == obj)) {
 				System.out.println(obj + "の勝ちだ！");
 				status = "finish";
@@ -96,6 +87,30 @@ public class OXgame {
 			status = "continue";
 		}
 		return status;
+	}
+
+	public static boolean horaizontalCheck(String[][] field, String obj) {
+//		横確認
+		int x = 0;
+		int y = 0;
+		boolean answer = false;
+		for(y = 0; y < 3; y++) {
+			answer = field[y][x] == obj && field[y][x + 1] == obj && field[y][x + 2] == obj;
+			if(answer == true) {break;}
+		}
+		return answer;
+	}
+
+	public static boolean varticalCheck(String[][] field, String obj) {
+//		縦確認
+		int x = 0;
+		int y = 0;
+		boolean answer = false;
+		for(x = 0; x < 3; x++) {
+			answer = field[y][x] == obj && field[y + 1][x] == obj && field[y + 2][x] == obj;
+			if(answer == true) {break;}
+		}
+		return answer;
 	}
 }
 
